2e154d6607a4c1f5c08e070fb5e67d4350eb5b5d
Christian Kleinboelting <kleinboelting@signal7.de>
2018-12-06 14:30:28 +0100

    Initial import

---
+++ src/main/java/net/signal7/demo/jx/jxdemo/DemoApplication.java
@@ -0,0 +1,12 @@
+package net.signal7.demo.jx.jxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ src/test/java/net/signal7/demo/jx/jxdemo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package net.signal7.demo.jx.jxdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
